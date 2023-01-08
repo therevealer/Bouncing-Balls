df1ff611cd35d2340ddb45a05f1d1a996435d43a
Setup <setup@Setups-MacBook-Pro.local>
2023-01-08 01:18:57 +0100

    Debugging random color

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 
-// Setting global variables
-let gravity = 0.99;
-dimension.strokeWidth = 5; // check for bugs
-
 
 // Setting up canvas in the browser.
 let canvas = document.getElementById('canvas');
-let dimension = canvas.getContext('2d');
-let mainWidth = window.innerWidth;
-let mainHeight = window.innerHeight;
-canvas.width = mainWidth;
-canvas.height = mainHeight;
+let c = canvas.getContext('2d');
+let tx = window.innerWidth;
+let ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
 
 
 // Declaring the variables and adding event listener for the mouse positions.
-let mouseX;
-let mouseY;
+let mouseX = 0;
+let mouseY = 0;
 
 addEventListener('mousemove', function(event){
     mouseX = event.clientX
     mouseY = event.clientY
 });
+
+
+// Setting global variables
+let grav = 0.99;
+c.strokeWidth = 5;
+
 
 
 // Setting the RGBA color for the balls
@@ -44,19 +46,72 @@ function Ball(){
     this.color = randomColor();
     this.radius = Math.random() * 20 + 14;
     this.startRadius = this.radius;
-    this.xcoordinate = Math.random() * (mainWidth - this.radius * 2) + this.radius;
-    this.ycoordinate = Math.random() * (mainHeight - this.radius);
+    this.x = Math.random() * (tx - this.radius * 2) + this.radius;
+    this.y = Math.random() * (ty - this.radius);
     this.dx = Math.random((Math.random() - 0.5) * 10);
     this.dy = Math.random() * 2;
     this.vel = Math.random() / 5;
     this.update = function() {
-        dimension.beginPath();
-        dimension.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        dimension.fillStyle = this.color;
-        dimension.fill();
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        c.fillStyle = this.color;
+        c.fill();
     };
 }
 
+// Setting up the for loop
+let bal = [];
+for (let i=0; i<40; i++){
+    bal.push(new Ball());
+}
+
+// The Ball Animation!!! POV : I don't understand this one.
+
+function animate() {    
+    if (tx != window.innerWidth || ty != window.innerHeight) {
+      tx = window.innerWidth;
+      ty = window.innerHeight;
+      canvas.width = tx;
+      canvas.height = ty;
+    }
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, tx, ty);
+    for (let i = 0; i < bal.length; i++) {
+      bal[i].update();
+      bal[i].y += bal[i].dy;
+      bal[i].x += bal[i].dx;
+      if (bal[i].y + bal[i].radius >= ty) {
+        bal[i].dy = -bal[i].dy * grav;
+      } else {
+        bal[i].dy += bal[i].vel;
+      }    
+      if(bal[i].x + bal[i].radius > tx || bal[i].x - bal[i].radius < 0){
+          bal[i].dx = -bal[i].dx;
+      }
+      if(mouseX > bal[i].x - 20 && 
+        mouseX < bal[i].x + 20 &&
+        mouseY > bal[i].y -50 &&
+        mouseY < bal[i].y +50 &&
+        bal[i].radius < 70){
+          //bal[i].x += +1;
+          bal[i].radius +=5; 
+        } else {
+          if(bal[i].radius > bal[i].startradius){
+            bal[i].radius += -5;
+          }
+        }
+        
+      //forloop end
+      }
+  //animation end
+  }
+  
+  animate();
+  
+  setInterval(function() {
+    bal.push(new Ball());
+    bal.splice(0, 1);
+  }, 400);
 
 
 
